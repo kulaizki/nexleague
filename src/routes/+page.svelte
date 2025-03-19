37e883ad8373601e122aed227ab1f3539d92273b
kulaizki <kulaizke@gmail.com>
2025-03-20 00:22:14 +0800

    feat: add initial hero section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { onMount } from 'svelte';
+  import { cubicOut } from 'svelte/easing';
+
+  let show: boolean = false;
+  let hideIntro: boolean = false;
+
+  function blurFly(
+    node: HTMLElement,
+    params: {
+      delay?: number;
+      duration?: number;
+      easing?: (t: number) => number;
+    } = {}
+  ): {
+    delay: number;
+    duration: number;
+    easing: (t: number) => number;
+    css: (t: number) => string;
+  } {
+    const existingTransform = getComputedStyle(node).transform.replace('none', '');
+    return {
+      delay: params.delay || 0,
+      duration: params.duration || 1500,
+      easing: params.easing || cubicOut,
+      css: (t: number) => `
+        transform: ${existingTransform} translateY(${(1 - t) * 100}px);
+        opacity: ${t};
+        filter: blur(${(1 - t) * 10}px);
+      `
+    };
+  }
+
+  onMount(() => {
+    show = true;
+  });
+
+  function handleHideIntro() {
+    hideIntro = true;
+  }
+</script>
+
+<div class="flex flex-col min-h-screen">
+  <section class="flex-grow flex flex-col items-center justify-center p-8 bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
+    {#if show}
+      <div class="max-w-7xl md:max-w-4xl text-center" transition:blurFly>
+        {#if !hideIntro}
+          <h1 class="mb-4 text-4xl font-bold tracking-tight md:text-7xl">
+            welcome to <span class="text-green-500 [text-shadow:0_0_8px_rgba(56,189,248,0.7)]">nexleague</span>
+          </h1>
+          <p class="mb-6 text-lg text-gray-300 md:text-xl">
+            Let's improve in League of Legends.
+          </p>
+        {/if}
+      </div>
+    {/if}
+  </section>
+  <!-- <Footer  /> -->
+</div>
