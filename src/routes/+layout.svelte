62feba33097ea0fcb530cb65551cd385e873af19
kulaizki <kulaizke@gmail.com>
2025-04-07 14:27:37 +0800

    style: update main layout background gradient for improved visual appeal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="flex flex-col min-h-screen">
-	<main class="flex-grow flex flex-col bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white">
+	<main class="flex-grow flex flex-col bg-gradient-to-br from-gray-950 via-gray-800 to-sky-400 text-white">
 		{@render children()}
 	</main>
 	<Footer />
